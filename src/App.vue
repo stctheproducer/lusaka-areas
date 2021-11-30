16de104c5af37c09c72a55051ea6cd2b36e2e891
Chanda Mulenga <dev@stctheproducer.com>
2021-11-30 22:13:35 +0200

    feat: add opengraph and twitter seo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import { appConstant } from '@/constants/appConstant'
+const { locale } = useI18n()
+
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
   title: appConstant.NAME,
-  meta: [{ name: 'description', content: appConstant.DESCRIPTION }],
+  meta: [
+    { name: 'description', content: appConstant.DESCRIPTION },
+    { name: 'twitter:card', content: 'summary' },
+    // { name: 'twitter:site', content: appConstant.TWITTER_SITE },
+    { name: 'twitter:creator', content: appConstant.TWITTER_CREATOR },
+    { property: 'og:url', content: appConstant.URL },
+    { property: 'og:site_name', content: appConstant.NAME },
+    { property: 'og:locale', content: locale.value },
+    { property: 'og:title', content: appConstant.NAME },
+    { property: 'og:type', content: appConstant.OG_TYPE },
+    { property: 'og:description', content: appConstant.DESCRIPTION },
+    { property: 'og:image', content: '@/assets/undraw_map_dark_re_36sy.svg' },
+    // { property: 'og:audio', content: '' },
+    // { property: 'og:video', content: '' },
+  ],
 })
 </script>
 
--- src/constants/appConstant.ts
+++ src/constants/appConstant.ts
@@ -2,6 +2,10 @@ export enum appConstant {
   NAME = 'Lusaka Areas',
   SHORT_NAME = 'Lusaka Areas',
   DESCRIPTION = 'A list of areas within Lusaka. Nothing major, just a small website for vibes.',
+  URL = 'https://lsk.stctheproducer.com',
+  TWITTER_SITE = '',
+  TWITTER_CREATOR = '@stctheproducer',
+  OG_TYPE = 'website',
   THEME_COLOR = '#ffffff',
 }
 
